f156781c16c49d3ec3ed37dd0922f7a663c23d9e
Maximilian Bese <maximilianbese@gmail.com>
2026-09-01 23:06:37 +0200

    refactor: simplify App component and add product data structure

---
+++ src/data/products.ts
@@ -0,0 +1,10 @@
+import type { Product } from "../types";
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Taufkerze",
+    description: "Personalisierte Taufkerze",
+    price: 0,
+  },
+];
